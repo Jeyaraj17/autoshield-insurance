6af9119dc4b347693fd102fa72784cb69d2db4c0
J <2439711@cognizant.com>
2026-06-18 15:38:42 +0530

    update frontend API URL to production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8082",
+  baseURL: "https://autoshield-insurance.onrender.com",
   withCredentials: true,
 });
 
